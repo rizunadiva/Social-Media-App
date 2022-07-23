1d5545dbb2766554d18a9e1850b7a363197632ab
rizunadiva <rizuananm@gmail.com>
2022-07-23 18:35:07 +0700

    config, query, logic, model, domain

--- domain/user.go
+++ domain/user.go
@@ -1,1 +1,31 @@
 package domain
+
+import "time"
+
+type User struct {
+	ID        int
+	UserName  string
+	Password  string
+	FullName  string
+	Photo     string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type UserUseCase interface {
+	AddUser(newUser User) (User, error)
+	GetAll() ([]User, error)
+	GetProfile(id int) (User, error)
+	UpdateUser(id int, updateProfile User) User
+	DeleteUser(id int) (row int, err error)
+	LoginUser(email string, password string) (token string, nama string, id int, errLog error)
+}
+
+type UserData interface {
+	Insert(newUser User) (User, error)
+	GetAll() ([]User, error)
+	GetSpecific(userID int) (User, error)
+	Update(userID int, updatedData User) User
+	Delete(userID int) (row int, err error)
+	Login(email string, password string) (token string, nama string, id int, err error)
+}
--- feature/user/data/model.go
+++ feature/user/data/model.go
@@ -1,1 +1,48 @@
 package data
+
+import (
+	"socialmedia-app/domain"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" form:"username" validate:"required,username"`
+	Password string `json:"password" form:"email" validate:"required"`
+	FullName string `json:"fullname" form:"fullname" validate:"required"`
+	Photo    string `json:"image_url"`
+	// News     []data.News `gorm:"foreignKey:By"`
+}
+
+func (u *User) ToModel() domain.User {
+	return domain.User{
+		ID:        int(u.ID),
+		UserName:  u.Username,
+		Password:  u.Password,
+		FullName:  u.FullName,
+		Photo:     u.Photo,
+		CreatedAt: u.CreatedAt,
+		UpdatedAt: u.UpdatedAt,
+	}
+}
+
+func ParseToArr(arr []User) []domain.User {
+	var res []domain.User
+
+	for _, val := range arr {
+		res = append(res, val.ToModel())
+	}
+
+	return res
+}
+
+func FromModel(data domain.User) User {
+	var res User
+	res.Username = data.UserName
+	res.Password = data.Password
+	res.FullName = data.FullName
+	res.Photo = data.Photo
+	// res.ID = data.ID
+	return res
+}
