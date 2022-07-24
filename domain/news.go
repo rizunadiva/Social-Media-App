ea2803fb870621bb93543c9c6bb4a409ecc5cf39
reski-id <pesan.reski@gmail.com>
2022-07-24 13:49:57 +0700

    domain/news

---
+++ domain/news.go
@@ -0,0 +1,25 @@
+package domain
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type News struct {
+	gorm.Model
+	ID             int
+	Conten         string
+	Images         string
+	FileAttachment string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type NewsUseCase interface {
+	AddNews(newNews News) (News, error)
+}
+
+type NewsData interface {
+	Insert(newNews News) (News, error)
+}
