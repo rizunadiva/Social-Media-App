5ee2d2d475b0f216d4d12b338d345c897bd87331
reski-id <pesan.reski@gmail.com>
2022-07-25 09:32:14 +0700

    add news domain

--- domain/news.go
+++ domain/news.go
@@ -3,12 +3,13 @@ package domain
 import (
 	"time"
 
+	"github.com/labstack/echo"
 	"gorm.io/gorm"
 )
 
 type News struct {
 	gorm.Model
-	ID             int
+	IDNews         int
 	Content        string
 	Images         string
 	FileAttachment string
@@ -16,10 +17,26 @@ type News struct {
 	UpdatedAt      time.Time
 }
 
+type newsHandler interface {
+	InsertNews() echo.HandlerFunc
+	DeleteNews() echo.HandlerFunc
+	UpdateNews() echo.HandlerFunc
+	GetAllNews() echo.HandlerFunc
+	GetMyNews() echo.HandlerFunc
+}
+
 type NewsUseCase interface {
-	AddNews(newNews News) (News, error)
+	GetAllM() ([]News, error)
+	GetMyM(IDUser int) ([]News, error)
+	AddBook(IDUser int, newNews News) (News, error)
+	DelBook(IDNews int) (bool, error)
+	UpBook(IDNews int, updateData News) (News, error)
 }
 
 type NewsData interface {
-	Insert(newNews News) (News, error)
+	GetAll() []News
+	GetMy(IDUser int) []News
+	Insert(newNews News) News
+	Delete(IDNews int) bool
+	Update(IDNews int, updatedNews News) News
 }
