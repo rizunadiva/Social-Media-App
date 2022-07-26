3da2d7513677cff7f5086b1098ac3202961d2b5f
reski-id <pesan.reski@gmail.com>
2022-07-27 00:03:50 +0700

    fitur commentar user file comment/delivery.request

--- feature/comment/delivery/request.go
+++ feature/comment/delivery/request.go
@@ -2,16 +2,12 @@ package delivery
 
 import "socialmedia-app/domain"
 
-type NewsInsertRequest struct {
-	Content        string `json:"content" form:"content"`
-	Images         string `json:"images" form:"images"`
-	FileAttachment string `json:"file" form:"file"`
+type commentsInsertRequest struct {
+	Content string `json:"content" form:"content"`
 }
 
-func (ni *NewsInsertRequest) ToDomain() domain.News {
-	return domain.News{
-		Content:        ni.Content,
-		Images:         ni.Images,
-		FileAttachment: ni.FileAttachment,
+func (ci *commentsInsertRequest) ToDomain() domain.Comments {
+	return domain.Comments{
+		Content: ci.Content,
 	}
 }
