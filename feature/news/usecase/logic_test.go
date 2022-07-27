37d168b11df786bc080f36cfd31dec6a2e030166
reski-id <pesan.reski@gmail.com>
2022-07-27 11:29:43 +0700

    skleton testing

---
+++ feature/news/usecase/logic_test.go
@@ -0,0 +1,19 @@
+package usecase
+
+import "testing"
+
+func testAddNews(t *testing.T) {
+
+}
+func testGetAllN(t *testing.T) {
+
+}
+func testGetMyN(t *testing.T) {
+
+}
+func testDelNews(t *testing.T) {
+
+}
+func testUpNews(t *testing.T) {
+
+}
--- feature/user/usecase/logic_test.go
+++ feature/user/usecase/logic_test.go
@@ -1,1 +1,19 @@
 package usecase
+
+import "testing"
+
+func testAddUser(t *testing.T) {
+
+}
+func testUpdateUser(t *testing.T) {
+
+}
+func testLoginUser(t *testing.T) {
+
+}
+func testDeleteUser(t *testing.T) {
+
+}
+func testGetProfile(t *testing.T) {
+
+}
